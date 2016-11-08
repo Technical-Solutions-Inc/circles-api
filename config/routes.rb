f232b4c6bf5f6757d2dd83171cf494f44c9b0184
borourke <borourke@crowdflower.com>
2016-11-07 16:34:33 -0800

    checkpoint with spec first TDD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'api/events', to: 'events#index', as: :events
+
+  # Ping route, to monitor health/uptime of the app
   get 'api/ping', to: 'pings#ping', as: :ping
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe EventsController do
-  it 'returns .ping_answer' do
-    get :ping
-    expect(response.body).to eq(expected_ping_answer)
+  describe "index" do
+    context "when no pagination is passed" do
+      let(:users) { create_list(:user, 3) }
+      let(:events) { create_list(:event, 2, users: users) }
+      let(:expected_payload) do
+        {
+          
+        }
+      end
+
+      it 'returns the expected payload' do
+        get :index, user_id: user.id
+        expect(response.status).to eq(200)
+        expect(response.body).to eq(expected_payload)
+      end
+    end
   end
 end
