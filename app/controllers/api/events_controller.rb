eba7621308a4bc2b1dfab51505d195a06439ec05
borourke <borourke@crowdflower.com>
2016-11-25 15:30:36 -0800

    resolve n+1 query problem

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -6,7 +6,12 @@ class Api::EventsController < ApplicationController
   end
 
   def get_events
-    User.find(params[:user_id]).events.offset(start_id).limit(count)
+    # Do not remove the "includes". This allows us to eager load the
+    # users association for each event.  Thereby avoiding
+    # the n+1 query problem because of the JBuilder view where
+    # each event is iterated over and the associated users (members)
+    # are included in the response payload.
+    User.find(params[:user_id]).events.includes(:users).offset(start_id).limit(count)
   end
 
   def default_count
